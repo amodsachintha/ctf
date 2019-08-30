10322fa4a75788d10ede68d1ec5361ba42db4a86
amodsachintha <amod077@gmail.com>
2019-08-30 10:38:53 +0530

    init

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/custom', function (){
+    return "Hello Laravel";
+});
+
+
